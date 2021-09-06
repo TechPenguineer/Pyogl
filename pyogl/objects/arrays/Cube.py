12f4888e3f7dfb98a2814138dac89f485521e26b
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-06 00:30:53 -0400

    all potential cube matrix

--- pyogl/objects/arrays/Cube.py
+++ pyogl/objects/arrays/Cube.py
@@ -1,4 +1,15 @@
+#    [ 1, 0, 0 ]
+#    [ 0, 0, 1 ]
+#    [ 0, 1, 0 ]
+#    [ 1, 0, 1 ]
+#    [ 0, 1, 1 ]
+#    [ 1, 1, 0 ]
+#    [ 1, 1, 1 ]
+#    [ 0, 0, 0 ] 
 class Matrix_Array:
     def Cube_Array(x_size, y_size, z_size):
+        x = x_size
+        y = y_size 
+        z = z_size
         
         
